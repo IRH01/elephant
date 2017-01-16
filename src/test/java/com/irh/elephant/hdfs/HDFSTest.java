c5eaf57221265b31ea9bd182c6510e8e8fd40d12
iritchie <469656844@qq.com>
2017-01-16 09:58:15 +0800

    更新

--- src/test/java/com/irh/elephant/hdfs/HDFSTest.java
+++ src/test/java/com/irh/elephant/hdfs/HDFSTest.java
@@ -1,14 +1,18 @@
 package com.irh.elephant.hdfs;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.*;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.FsUrlStreamHandlerFactory;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IOUtils;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 
 /**
@@ -16,95 +20,62 @@ import java.net.URL;
  */
 public class HDFSTest{
 
-    //initialization
-//    private Configuration conf = new Configuration();
-//    private FileSystem hdfs;
-
-//    @BeforeTest
-//    public void setUp() throws IOException{
-//        String path = "/usr/program_data/idea/elephant/conf";
-//        conf.addResource(new Path(path + "core-site.xml"));
-//        conf.addResource(new Path(path + "hdfs-siete.xml"));
-//        conf.addResource(new Path(path + "mapred-site.xml"));
-////        path = "/usr/java/hbase-0.90.3/conf/";
-////        conf.addResource(new Path(path + "hbase-site.xml"));
-//        hdfs = FileSystem.get(conf);
-//    }
-
     @Test
-    public void testOperator() throws IOException{
+    public void testQuery() throws IOException{
         URL.setURLStreamHandlerFactory(new FsUrlStreamHandlerFactory());
-        String path = "hdfs://192.168.105.9:9000/user/root/input/log4j.properties";
+        String path = "hdfs://192.168.105.9:9000/user/root/output/test.txt";
         InputStream in = new URL(path).openStream();
         OutputStream ou = System.out;
         int buffer = 4096;
         boolean close = false;
         IOUtils.copyBytes(in, ou, buffer, close);
-
         IOUtils.closeStream(in);
     }
 
-//    //create a direction
-//    @Test
-//    public void createDir() throws IOException{
-//        String dir = "/test";
-//        Path path = new Path(dir);
-//        hdfs.mkdirs(path);
-//        System.out.println("new dir \t" + conf.get("fs.default.name") + dir);
-//    }
-//
-//    //copy from local file to HDFS file
-//    @Test
-//    public void copyFile() throws IOException{
-//        String hdfsDst = "/test";
-//        String localSrc = "/home/ictclas/Configure.xml";
-//
-//        Path src = new Path(localSrc);
-//        Path dst = new Path(hdfsDst);
-//        hdfs.copyFromLocalFile(src, dst);
-//
-//        //list all the files in the current direction
-//        FileStatus files[] = hdfs.listStatus(dst);
-//        System.out.println("Upload to \t" + conf.get("fs.default.name") + hdfsDst);
-//        for(FileStatus file : files){
-//            System.out.println(file.getPath());
-//        }
-//    }
-//
-//    //create a new file
-//    @Test
-//    public void createFile() throws IOException{
-//        String fileName = "/test/word.txt";
-//        String fileContent = "Hello, world! Just a test.";
-//
-//        Path dst = new Path(fileName);
-//        byte[] bytes = fileContent.getBytes();
-//        FSDataOutputStream output = hdfs.create(dst);
-//        output.write(bytes);
-//        System.out.println("new file \t" + conf.get("fs.default.name") + fileName);
-//    }
-//
-//    //list all files
-//    public void listFiles(String dirName) throws IOException{
-//
-//        Path f = new Path(dirName);
-//        FileStatus[] status = hdfs.listStatus(f);
-//        System.out.println(dirName + " has all files:");
-//        for(int i = 0; i < status.length; i++){
-//            System.out.println(status[i].getPath().toString());
-//        }
-//    }
-//
-//    //judge a file existed? and delete it!
-//    public void deleteFile(String fileName) throws IOException{
-//        Path f = new Path(fileName);
-//        boolean isExists = hdfs.exists(f);
-//        if(isExists){    //if exists, delete
-//            boolean isDel = hdfs.delete(f, true);
-//            System.out.println(fileName + "  delete? \t" + isDel);
-//        }else{
-//            System.out.println(fileName + "  exist? \t" + isExists);
-//        }
-//    }
+    @Test
+    public void testCopyLocalFile() throws IOException, URISyntaxException{
+        Configuration conf = new Configuration();
+        FileSystem hdfs = FileSystem.get(conf);
 
+        Path src = new Path("input");
+        Path dest = new Path("output");
+
+        hdfs.copyFromLocalFile(src, dest);
+
+        System.err.println("Upload to :" + conf.get("fs.default.name"));
+
+        FileStatus files[] = hdfs.listStatus(dest);
+
+        for(FileStatus file : files){
+            System.err.println("结果：" + file.getPath());
+        }
+    }
+
+    @Test
+    public void testCopyLocalToRemoteFile() throws IOException, URISyntaxException, InterruptedException{
+        Configuration conf = new Configuration();
+        FileSystem hdfs = FileSystem.get(new URI("hdfs://192.168.105.9:9000/user/root/"), conf, "root");
+
+        Path src = new Path("input");
+        Path dest = new Path("output");
+
+        hdfs.copyFromLocalFile(src, dest);
+
+        System.err.println("Upload to :" + conf.get("fs.default.name"));
+
+        FileStatus files[] = hdfs.listStatus(dest);
+
+        for(FileStatus file : files){
+            System.err.println("结果：" + file.getPath());
+        }
+    }
+
+    @Test
+    public void testGetData() throws IOException{
+        Configuration conf = new Configuration();
+//        conf.addResource("/usr/program_data/idea/elephant/conf/core-site.xml");
+//        conf.addResource("/usr/program_data/idea/elephant/conf/hdfs-site.xml");
+
+        System.err.println(conf.get("hadoop.tmp.dir"));
+    }
 }
